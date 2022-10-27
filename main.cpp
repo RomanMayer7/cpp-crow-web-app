b6c7fbcc9e7179ccc6f76aaaac0a4bf9c8d7af8d
RomanMayer7 <romanmayerzon@gmail.com>
2022-10-27 22:06:46 +0300

    Serving basic static HTML content

--- main.cpp
+++ main.cpp
@@ -1,12 +1,35 @@
 #include "crow_all.h"
+#include <unistd.h>
 using namespace std;
+using namespace crow;
 
 int main(int argc, char* argv[]) {
   crow::SimpleApp app;
 
   CROW_ROUTE(app, "/")
-    ([](){
-      return "<div><h1>Hello, Crow.</h1></div>";
+    ([](const request &req,response &res){
+
+      char *cwd = get_current_dir_name();
+      cout << "Current working directory: " << cwd << endl;
+      free(cwd);;
+      string publicFolderPath="/usr/src/cppweb/hello_crow/public/index.html";
+      ifstream in(publicFolderPath,ifstream::in);
+      if( in.is_open() ){
+        cout<<"-----------static HTML content exist in public folder------------"<<endl;
+            //  in.close();
+          } else {
+              perror(publicFolderPath.c_str());
+          }
+
+      if(in){
+        ostringstream contents;
+        contents<<in.rdbuf();
+        in.close();
+        res.write(contents.str());
+      } else {
+        res.write("Not Found");
+      }
+      res.end();
     });
 
   char* port = getenv("PORT");
